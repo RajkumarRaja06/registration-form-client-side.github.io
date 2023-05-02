49c87fdb4edb09648cbbb4009e3d8ab631994359
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-05-02 21:31:59 +0530

    validation added

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,95 @@
-const userNameInput = document.getElementById('username');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const confirmPasswordInput = document.getElementById('confirmPassword');
-const button = document.getElementById('btn-Submit');
-const formEl = document.getElementById('form');
+const userNameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const button = document.getElementById("btn-Submit");
+const formEl = document.getElementById("form");
 
 function showError(input, message) {
   //parent element
-  const parentEl = input.parentElement
+  const parentEl = input.parentElement;
   //child element
-  const errorEl = parentEl.querySelector('.error');
+  const errorEl = parentEl.querySelector(".error");
   //update class
-  errorEl.classList.add('error-msg');
+  errorEl.classList.add("error-msg");
   //display in HTML
   errorEl.innerHTML = message;
 }
 
 function successMsg(input) {
-  input.classList.add('success')
+  input.classList.add("success");
   //parent element
-  const parentEl = input.parentElement
+  const parentEl = input.parentElement;
   //child element
-  const errorEl = parentEl.querySelector('.error');
+  const errorEl = parentEl.querySelector(".error");
   //update class
-  errorEl.classList.remove('error-msg');
+  errorEl.classList.remove("error-msg");
 }
 
 const array = [
   {
-    input : userNameInput,
-    message :"Username is Mandatory"
+    input: userNameInput,
+    message: "Username is Mandatory",
   },
   {
-    input : emailInput,
-    message :"Email is Mandatory"
+    input: emailInput,
+    message: "Email is Mandatory",
   },
   {
-    input : passwordInput,
-    message :"Password is Mandatory"
+    input: passwordInput,
+    message: "Password is Mandatory",
   },
   {
-    input : confirmPasswordInput,
-    message :"Confirm Password is Mandatory"
-  }
-]
+    input: confirmPasswordInput,
+    message: "Confirm Password is Mandatory",
+  },
+];
 
-const checkRequired = function(input, message) {
-  if(input.value.trim()) {
+const checkRequired = function (input, message) {
+  if (input.value.trim()) {
     // successMsg(input);
-  }
-  else {
+  } else {
     showError(input, `${message}`);
   }
-}
+};
 
-const validateInput = function(input, message, min, max) {
-
+const validateInput = function (input, message, min, max) {
   if (input.value.trim().length < min) {
     showError(input, `${message} should be at least ${min} character`);
-  }
-  else if (input.value.trim().length > max){
+  } else if (input.value.trim().length > max) {
     showError(input, `${message} should be less then ${max} character`);
-  }
-  else {
+  } else {
     successMsg(input);
   }
-}
+};
 
 function isEmailAddress(str) {
-  var pattern =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return pattern.test(str);  // returns a boolean 
+  var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return pattern.test(str); // returns a boolean
 }
 
-const validateEmail = function(input) {
+const validateEmail = function (input) {
   if (isEmailAddress(input.value.trim())) {
-    successMsg(input)
+    successMsg(input);
+  } else {
+    showError(input, "Email in proper format");
   }
-  else{
-    showError(input, "Email in proper format")
-  }
-}
+};
 
-const validateConfirmPassword = function(input1, input2) {
-  
-  if(input2.value.trim() === "") {
-    showError(input2, "Confirm Password is empty")
+const validateConfirmPassword = function (input1, input2) {
+  if (input2.value.trim() === "") {
+    showError(input2, "Confirm Password is empty");
+  } else if (input1.value.trim() === input2.value.trim()) {
+    successMsg(input2);
+  } else {
+    showError(input2, "Password and Confirm Password not match");
   }
-  else if (input1.value.trim() === input2.value.trim()) {
-    successMsg(input2)
-  }
-  else {
-    showError(input2, "Password and Confirm Password not match")
-  }
-} 
+};
 
-formEl.addEventListener('submit', function (event) {
+formEl.addEventListener("submit", function (event) {
   event.preventDefault();
   // debugger;
-  
+
   for (let i = 0; i < array.length; i++) {
     input = array[i].input;
     message = array[i].message;
@@ -109,6 +101,8 @@ formEl.addEventListener('submit', function (event) {
   validateEmail(emailInput);
   validateConfirmPassword(passwordInput, confirmPasswordInput);
 
-
-  
-})
+  userNameInput.value = null;
+  passwordInput.value = null;
+  emailInput.value = null;
+  confirmPasswordInput.value = null;
+});
